a574c153b99679db8498ef37540fd22bec99f423
jsdw14 <jsdw@icloud.com>
2023-05-08 18:52:56 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,35 @@
 input.onButtonPressed(Button.A, function () {
-    item += 1
-})
-let item = 0
-weatherbit.startWeatherMonitoring()
-basic.forever(function () {
-    if (item == 0) {
-        basic.showString("T: ")
+    while (item == 0) {
+        basic.showString("T:")
         basic.showNumber(Math.idiv(weatherbit.temperature(), 100))
-    } else if (item == 1) {
+    }
+    while (item == 1) {
         basic.showString("H: ")
         basic.showNumber(Math.idiv(weatherbit.humidity(), 1024))
-    } else if (item == 2) {
+    }
+    while (item == 2) {
         basic.showString("P: ")
         basic.showNumber(Math.idiv(weatherbit.pressure(), 25600))
-    } else {
-        item = 0
+    }
+    item = 0
+})
+input.onButtonPressed(Button.B, function () {
+    light2 = true
+    item += 1
+    while (light2 == true) {
+        lightVal = input.lightLevel()
+        led.plotBarGraph(
+        lightVal,
+        255
+        )
+        if (input.buttonIsPressed(Button.A)) {
+            light2 = false
+        }
     }
 })
+let lightVal = 0
+let light2 = false
+let item = 0
+item = 0
+light2 = false
+weatherbit.startWeatherMonitoring()
