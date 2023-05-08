f7457f98ded025b93b70db500d4f076880fd64f8
jsdw14 <jsdw@icloud.com>
2023-05-08 18:20:47 +1000

    shortened display text

--- main.ts
+++ main.ts
@@ -5,13 +5,13 @@ let item = 0
 weatherbit.startWeatherMonitoring()
 basic.forever(function () {
     if (item == 0) {
-        basic.showString("Temp C: ")
+        basic.showString("T")
         basic.showNumber(Math.idiv(weatherbit.temperature(), 100))
     } else if (item == 1) {
-        basic.showString("Humidity %: ")
+        basic.showString("H")
         basic.showNumber(Math.idiv(weatherbit.humidity(), 1024))
     } else if (item == 2) {
-        basic.showString("Pressure hPa: ")
+        basic.showString("P")
         basic.showNumber(Math.idiv(weatherbit.pressure(), 25600))
     } else {
         item = 0
